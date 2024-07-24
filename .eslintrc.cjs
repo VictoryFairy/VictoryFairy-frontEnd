7188a4497f288c6ae59e7dca27759b6312c54f79
phj <aaagowns@gmail.com>
2024-07-24 19:40:39 +0900

    기존에 있던 eslint-typescript 추가
    - 기존에 있던 거를 충돌날까봐 지웠는데 문제 없을 것 같아서 다시 추가했습니다.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -8,6 +8,7 @@ module.exports = {
     "airbnb-typescript",
     "airbnb/hooks",
     "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
     "plugin:prettier/recommended", // Prettier의 포맷팅 규칙을 ESLint 규칙으로 사용할 수 있게 해줌
     "plugin:react/jsx-runtime", // import React from 'react'를 해야 jsx를 사용할 수 있게 해주는데 이를 없애기 위해 사용
   ],
