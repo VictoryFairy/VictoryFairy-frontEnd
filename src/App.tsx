831a2c341bfc00834972e6a34a026082ce94e725
namkijoo <rlwn5904@naver.com>
2024-08-07 02:56:42 +0900

    ✨ Header nav 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,34 @@
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import styled from "styled-components";
 import Layout from "./components/layout/Layout";
+import { typography } from "./style/typography";
+
 // import { ThemeProvider } from "styled-components";
 
 const queryClient = new QueryClient();
 
+const LeftWrapper = styled.div`
+  ${typography.subtitle_02}
+`;
+
+const MiddelWrapper = styled.div`
+  ${typography.headline}
+`;
+
+const RightWrapper = styled.div`
+  ${typography.subtitle_02}
+`;
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Layout />,
+    element: (
+      <Layout
+        left={<LeftWrapper>left</LeftWrapper>}
+        center={<MiddelWrapper>Title</MiddelWrapper>}
+        right={<RightWrapper>Right</RightWrapper>}
+      />
+    ),
     children: [
       {
         path: "/",
