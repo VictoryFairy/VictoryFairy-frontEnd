ba00946527ed0c41a0cc139c459e1860b212ee36
minseok <imuchgabis@gmail.com>
2024-08-07 10:36:16 +0900

    💄 회원가입 페이지 단계별 적용
    회원가입 페이지 단계별 적용

---
+++ src/components/signup/EmailValid.tsx
@@ -0,0 +1,8 @@
+interface EmailValidProps {
+  setstep: (step: number) => void;
+}
+const EmailValid = ({ setstep }: EmailValidProps) => {
+  return <div>EmailValid</div>;
+};
+
+export default EmailValid;
--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,5 +1,26 @@
+import { useState } from "react";
+import EmailValid from "../components/signup/EmailValid";
+import VerificationCode from "../components/signup/VerificationCode";
+
 const Signup = () => {
-  return <div>Signup</div>;
+  const [step, setstep] = useState(1);
+  const renderStep = () => {
+    switch (step) {
+      case 1:
+        return <EmailValid setstep={setstep} />;
+      case 2:
+        return <VerificationCode />;
+      case 3:
+        return <div>ㅗㅇ</div>;
+      case 4:
+        return <div>ㅗㅇ</div>;
+      case 5:
+        return <div>ㅗㅇ</div>;
+      default:
+        return null;
+    }
+  };
+  return <div style={{ height: "100%" }}>{renderStep()}</div>;
 };
 
 export default Signup;
