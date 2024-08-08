cd562070765305a3cc28a34bb68f4389de11d1d5
namkijoo <rlwn5904@naver.com>
2024-08-09 01:36:29 +0900

    ✨ Footer padding 삭제

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -56,7 +56,6 @@ const FooterContainer = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
-  padding: 0 20px;
 `;
 
 const IconWrapper = styled.div<IconProps>`
