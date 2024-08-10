39e7fa4e511ce6775794a25f440f83b079675e0a
minseok <imuchgabis@gmail.com>
2024-08-10 18:27:17 +0900

    ✨ AuthAxios 인터셉터 추가 구현
    authAxios 인터셉터 추가 구현

--- src/api/authAxios.ts
+++ src/api/authAxios.ts
@@ -1,5 +1,7 @@
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 import { useAuthStore } from "../store/authStore";
+import axiosInstance from "./axios";
 
 const BASE_URL = import.meta.env.VITE_API_URL;
 
@@ -25,29 +27,27 @@ authAxiosInstance.interceptors.request.use(
 authAxiosInstance.interceptors.response.use(
   (response) => response,
   async (error) => {
-    const originalRequest = error.config;
+    const retryRequest = error.config;
     const { response } = error;
+    const navigate = useNavigate();
 
     if (response && response.status === 401) {
       try {
-        // 리프레시 토큰으로 액세스 토큰을 새로 발급 받습니다.
+        const refreshResponse = await axiosInstance.post(
+          `${BASE_URL}/auth/token/issue`,
+        );
 
-        const refreshResponse = await axios.post(`${BASE_URL}/auth/refresh`);
-
-        // 새 액세스 토큰을 스토어와 로컬스토리지에 저장합니다.
         const newAccessToken = refreshResponse.data.acToken;
         useAuthStore.getState().loginAction(newAccessToken);
 
-        // 원래의 요청을 새 액세스 토큰으로 다시 시도합니다.
-        authAxiosInstance.defaults.headers.Authorization = `Bearer ${newAccessToken}`;
-        originalRequest.headers.Authorization = `Bearer ${newAccessToken}`;
+        retryRequest.headers.Authorization = `Bearer ${newAccessToken}`;
 
-        return authAxiosInstance(originalRequest);
-      } catch (refreshError) {
-        // 리프레시 토큰이 만료되었거나 오류 발생 시 로그아웃 처리
+        return authAxiosInstance(retryRequest);
+      } catch (err) {
         // toast.error("로그인 세션이 만료되었습니다. 다시 로그인 해주세요.");
         useAuthStore.getState().logoutAction();
-        return Promise.reject(refreshError);
+        navigate("/");
+        return Promise.reject(err);
       }
     }
 
