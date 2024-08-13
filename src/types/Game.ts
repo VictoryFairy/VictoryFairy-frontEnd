49cc1991c1c6f5670103fa672ed13fe3a663d504
phj <aaagowns@gmail.com>
2024-08-13 19:17:39 +0900

    ✨ 직관 게임 타입 정의

---
+++ src/types/Game.ts
@@ -0,0 +1,38 @@
+export interface MyGame {
+  id: number;
+  image: string;
+  seat: string;
+  review: string;
+  status: "WIN" | "Lose" | "Tie" | "No game";
+  game: {
+    id: string;
+    date: string;
+    time: string;
+    status: string;
+    homeTeam: {
+      id: number;
+      name: string;
+    };
+    awayTeam: {
+      id: number;
+      name: string;
+    };
+    stadium: {
+      id: number;
+      name: string;
+      latitude: number;
+      longitude: number;
+      address: string;
+    };
+    homeTeamScore: number;
+    awayTeamScore: number;
+    winningTeam: {
+      id: number;
+      name: string;
+    };
+  };
+  cheeringTeam: {
+    id: number;
+    name: string;
+  };
+}
