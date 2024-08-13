7fcf6cd61fcae87a6e5277d3c4dc45e9f81ed2cf
minseok <imuchgabis@gmail.com>
2024-08-13 17:46:50 +0900

    💄 맵 페이지 api 연결후 수정
    맵 페이지 api 연결후 수정

--- src/components/info/map/ParkingInfoPage.tsx
+++ src/components/info/map/ParkingInfoPage.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useQuery } from "@tanstack/react-query";
 import StadiumList from "./StadiumList";
 import Map from "./Map";
@@ -18,63 +18,30 @@ const ParkingInfoPage = () => {
     "stadium"
   > | null>(null);
 
-  // const { data } = useQuery<ParkingInfo[]>({
-  //   queryKey: ["parkingInfos", selectedStadiumId],
-  //   queryFn: () => getParkingInfosByStadiumId(selectedStadiumId),
-  // });
+  const { data } = useQuery<ParkingInfo[]>({
+    queryKey: ["parkingInfos", selectedStadiumId],
+    queryFn: () => getParkingInfosByStadiumId(selectedStadiumId),
+    enabled: !!selectedStadiumId,
+  });
 
-  // useEffect(() => {
-  //   if (data) {
-  //     setSelectedParking(null);
-  //   }
-  // }, [data]);
-  // if (!data) return;
-  // const stadiumData = data?.[0].stadium;
-  // const parkingSpots = data?.map((parkingInfo) => ({
-  //   id: parkingInfo.id,
-  //   name: parkingInfo.name,
-  //   is_free: parkingInfo.is_free,
-  //   latitude: parkingInfo.latitude,
-  //   longitude: parkingInfo.longitude,
-  //   address: parkingInfo.address,
-  // }));
-  const stadiumData = {
-    id: 1,
-    name: "잠실",
-    latitude: 35.8409,
-    longitude: 128.6828,
-    address: "잠실",
-  };
-  const parkingSpots = [
-    {
-      id: 1,
-      address: "대구 수성구 알파시티1로 232",
-      name: "데이터뱅크 주차장",
-      latitude: 35.83883,
-      longitude: 128.68388,
-    },
-    {
-      id: 2,
-      address: "대구 수성구 대구미술관",
-      name: "미술관 주차장 주차장",
-      latitude: 35.8276258,
-      longitude: 128.6739499,
-    },
-    {
-      id: 3,
-      address: "대구 수성구 대구미술관",
-      name: "미술관 주차장 주차장",
-      latitude: 35.8276258,
-      longitude: 128.6739499,
-    },
-    {
-      id: 4,
-      address: "대구 수성구 대구미술관",
-      name: "미술관 주차장 주차장",
-      latitude: 35.8276258,
-      longitude: 128.6739499,
-    },
-  ];
+  useEffect(() => {
+    if (data) {
+      setSelectedParking(null);
+    }
+  }, [data]);
+  if (!data) return;
+
+  console.log(data);
+  const stadiumData = data?.[0].stadium;
+  const parkingSpots = data?.map((parkingInfo) => ({
+    id: parkingInfo.id,
+    name: parkingInfo.name,
+    latitude: parkingInfo.latitude,
+    longitude: parkingInfo.longitude,
+    address: parkingInfo.address,
+    link: parkingInfo.link,
+  }));
+
   return (
     <div>
       {/* <SearchBar>
