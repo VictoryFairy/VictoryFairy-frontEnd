7cae46afc679bb4f9b44e1ce0bf1d0ed987e3eeb
phj <aaagowns@gmail.com>
2024-08-15 05:20:00 +0900

    ♻️ 경기 타입 수정

--- src/types/Game.ts
+++ src/types/Game.ts
@@ -4,34 +4,36 @@ export interface MyGame {
   seat: string;
   review: string;
   status: "WIN" | "Lose" | "Tie" | "No game";
-  game: {
-    id: string;
-    date: string;
-    time: string;
-    status: string;
-    homeTeam: {
-      id: number;
-      name: string;
-    };
-    awayTeam: {
-      id: number;
-      name: string;
-    };
-    stadium: {
-      id: number;
-      name: string;
-      latitude: number;
-      longitude: number;
-      address: string;
-    };
-    homeTeamScore: number;
-    awayTeamScore: number;
-    winningTeam: {
-      id: number;
-      name: string;
-    };
-  };
+  game: Game;
   cheeringTeam: {
+    id: number;
+    name: string;
+  };
+}
+
+export interface Game {
+  id: string;
+  date: string;
+  time: string;
+  status: string;
+  homeTeam: {
+    id: number;
+    name: string;
+  };
+  awayTeam: {
+    id: number;
+    name: string;
+  };
+  stadium: {
+    id: number;
+    name: string;
+    latitude: number;
+    longitude: number;
+    address: string;
+  };
+  homeTeamScore: number;
+  awayTeamScore: number;
+  winningTeam: {
     id: number;
     name: string;
   };
