ca539a5484b4eca0edc133d049793fe81b39d67f
minseok <imuchgabis@gmail.com>
2024-08-16 12:00:58 +0900

    ✨ 인피니트 스크롤 데이터 변수명 변경, 좋아요리스트 추가,empty추가
    인피니트 스크롤 데이터 변수명 변경, 좋아요리스트 추가,empty추가

--- src/components/info/cheerSong/CheerSongPage.tsx
+++ src/components/info/cheerSong/CheerSongPage.tsx
@@ -12,11 +12,13 @@ import {
   fetchCheerSongs,
   FetchLikedCheerSongs,
 } from "../../../api/info/cheers.api";
+import empty from "../../../assets/images/Group 625793.png";
+import Text from "../../common/Text";
 
 const CheerSongPage = () => {
   const { teamId } = useAuthStore();
   const [selectedTeamId, setSelectedTeamId] = useState(teamId);
-  const [activeTab, setActiveTab] = useState(0); // 0 team, 1 player
+  const [activeTab, setActiveTab] = useState(0); // 0: team, 1: player
 
   const navigate = useNavigate();
   const observer = useRef<IntersectionObserver | null>(null);
@@ -29,54 +31,72 @@ const CheerSongPage = () => {
     });
   }, [selectedTeamId, activeTab, queryClient]);
 
-  const { data2, fetchNextPage2, hasNextPage2, isFetchingNextPage2 } =
-    useInfiniteQuery({
-      queryKey: ["LikedcheerSongs"],
-      queryFn: async ({ pageParam = 0 }) => {
-        const response = await FetchLikedCheerSongs({
-          pageParam,
-        });
-        return response;
-      },
-      getNextPageParam: (lastPage) => {
-        return lastPage.meta.hasNextData ? lastPage.meta.cursor : undefined;
-      },
-      initialPageParam: 0,
-      enabled: selectedTeamId === 0,
-      select: (data3) => ({
-        pages: data3.pages.flatMap((page) => page.data),
-      }),
-    });
+  const {
+    data: likedCheerSongsData,
+    fetchNextPage: fetchNextLikedPage,
+    hasNextPage: hasNextLikedPage,
+    isFetchingNextPage: isFetchingNextLikedPage,
+  } = useInfiniteQuery({
+    queryKey: ["likedCheerSongs"],
+    queryFn: async ({ pageParam = 0 }) => {
+      const response = await FetchLikedCheerSongs({
+        pageParam,
+      });
+      return response;
+    },
+    getNextPageParam: (lastPage) => {
+      return lastPage.meta.hasNextData ? lastPage.meta.cursor : undefined;
+    },
+    initialPageParam: 0,
+    enabled: selectedTeamId === 0,
+    select: (data) => ({
+      pages: data.pages.flatMap((page) => page.data),
+    }),
+  });
 
-  const { data, fetchNextPage, hasNextPage, isFetchingNextPage } =
-    useInfiniteQuery({
-      queryKey: ["cheerSongs", selectedTeamId, activeTab],
-      queryFn: async ({ pageParam = 0 }) => {
-        const type = activeTab === 0 ? "team" : "player";
-        const response = await fetchCheerSongs({
-          pageParam,
-          teamId: selectedTeamId,
-          type,
-        });
-        return response;
-      },
-      getNextPageParam: (lastPage) => {
-        return lastPage.meta.hasNextData ? lastPage.meta.cursor : undefined;
-      },
-      initialPageParam: 0,
-      enabled: selectedTeamId !== 0,
-      select: (data2) => ({
-        pages: data2.pages.flatMap((page) => page.data),
-      }),
-    });
+  const {
+    data: cheerSongsData,
+    fetchNextPage: fetchNextCheerPage,
+    hasNextPage: hasNextCheerPage,
+    isFetchingNextPage: isFetchingNextCheerPage,
+  } = useInfiniteQuery({
+    queryKey: ["cheerSongs", selectedTeamId, activeTab],
+    queryFn: async ({ pageParam = 0 }) => {
+      const type = activeTab === 0 ? "team" : "player";
+      const response = await fetchCheerSongs({
+        pageParam,
+        teamId: selectedTeamId,
+        type,
+      });
+      return response;
+    },
+    getNextPageParam: (lastPage) => {
+      return lastPage.meta.hasNextData ? lastPage.meta.cursor : undefined;
+    },
+    initialPageParam: 0,
+    enabled: selectedTeamId !== 0,
+    select: (data) => ({
+      pages: data.pages.flatMap((page) => page.data),
+    }),
+  });
 
   useEffect(() => {
     if (observer.current) observer.current.disconnect();
 
     const handleObserver = (entries: IntersectionObserverEntry[]) => {
       const target = entries[0];
-      if (target.isIntersecting && hasNextPage && !isFetchingNextPage) {
-        fetchNextPage();
+      if (
+        target.isIntersecting &&
+        hasNextCheerPage &&
+        !isFetchingNextCheerPage
+      ) {
+        fetchNextCheerPage();
+      } else if (
+        target.isIntersecting &&
+        hasNextLikedPage &&
+        !isFetchingNextLikedPage
+      ) {
+        fetchNextLikedPage();
       }
     };
 
@@ -91,7 +111,14 @@ const CheerSongPage = () => {
     return () => {
       if (observer.current) observer.current.disconnect();
     };
-  }, [fetchNextPage, hasNextPage, isFetchingNextPage]);
+  }, [
+    fetchNextCheerPage,
+    hasNextCheerPage,
+    isFetchingNextCheerPage,
+    fetchNextLikedPage,
+    hasNextLikedPage,
+    isFetchingNextLikedPage,
+  ]);
 
   return (
     <Container>
@@ -113,21 +140,59 @@ const CheerSongPage = () => {
         />
       </div>
 
-      {data?.pages.map((cheerSong) => (
-        <CheerSongList
-          key={cheerSong.id}
-          id={cheerSong.id}
-          teamName={cheerSong.team.name as TeamName}
-          title={cheerSong.title}
-          lyricPreview={cheerSong.lyrics_preview}
-          isLiked={cheerSong.isLiked}
-          jerseyNumber={
-            cheerSong.player
-              ? cheerSong.player.jerseyNumber.toString()
-              : undefined
-          }
-        />
-      ))}
+      {selectedTeamId === 0 && (
+        <div>
+          {likedCheerSongsData?.pages.length === 0 ? (
+            <EmptyState>
+              <img src={empty} alt='No liked cheer songs' />
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  alignItems: "center",
+                  gap: "5px",
+                }}>
+                <Text variant='subtitle_03'>My응원가가 없어요</Text>
+                <Text>하트를 눌러 등록해보세요</Text>
+              </div>
+            </EmptyState>
+          ) : (
+            likedCheerSongsData?.pages.map((cheerSong) => (
+              <CheerSongList
+                key={cheerSong.id}
+                id={cheerSong.id}
+                teamName={cheerSong.team.name as TeamName}
+                title={cheerSong.title}
+                lyricPreview={cheerSong.lyrics_preview}
+                isLiked={cheerSong.isLiked}
+                jerseyNumber={
+                  cheerSong.player
+                    ? cheerSong.player.jerseyNumber.toString()
+                    : undefined
+                }
+              />
+            ))
+          )}
+        </div>
+      )}
+
+      {selectedTeamId !== 0 &&
+        cheerSongsData?.pages.map((cheerSong) => (
+          <CheerSongList
+            key={cheerSong.id}
+            id={cheerSong.id}
+            teamName={cheerSong.team.name as TeamName}
+            title={cheerSong.title}
+            lyricPreview={cheerSong.lyrics_preview}
+            isLiked={cheerSong.isLiked}
+            jerseyNumber={
+              cheerSong.player
+                ? cheerSong.player.jerseyNumber.toString()
+                : undefined
+            }
+          />
+        ))}
+
       <div ref={lastElementRef} className='trigger' />
     </Container>
   );
@@ -154,6 +219,7 @@ const Container = styled.div`
     margin-bottom: 60px;
   }
 `;
+
 const SearchBarWrapper = styled.div`
   display: flex;
   align-items: center;
@@ -177,6 +243,15 @@ const SearchInput = styled.input`
 const SearchIcon = styled.img`
   color: #888;
   cursor: pointer;
+`;
+
+const EmptyState = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  justify-content: center;
+  align-items: center;
+  height: 200px;
 `;
 
 export default CheerSongPage;
