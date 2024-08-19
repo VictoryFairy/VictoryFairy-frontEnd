3d7694e2548245a645e80b4a036d3a55dd184ba5
phj <aaagowns@gmail.com>
2024-08-19 22:21:15 +0900

    🔧 클릭 시 세부페이지 이동 추가

--- src/components/watchList/WatchList.tsx
+++ src/components/watchList/WatchList.tsx
@@ -1,7 +1,11 @@
 import styled from "styled-components";
 import { useState } from "react";
 import { typography } from "@/style/typography";
+import { getRegisteredGameMonthly } from "@/api/register/register";
+import { useQuery } from "@tanstack/react-query";
 import { MyGame } from "@/types/Game";
+import moment from "moment";
+import { useNavigate } from "react-router-dom";
 import ListTab from "./ListTab/ListTab";
 import FilterIcon from "@/assets/Icons/filter.svg?react";
 import SearchIcon from "@/assets/Icons/search.svg?react";
@@ -9,178 +13,65 @@ import SelectionBar from "../common/SelectionBar";
 import CalendarContainer from "../common/Calendar";
 import GalleryTab from "./GalleryTab/GalleryTab";
 
-const DATA: MyGame[] = [
-  {
-    id: 1,
-    image:
-      "https://i.pinimg.com/736x/82/72/97/82729707c6b933e8b7fce187fe0adb1d.jpg",
-    seat: "115블록 2열 13번",
-    review: "좋았다",
-    status: "WIN",
-    game: {
-      id: "20240801SSLG0",
-      date: "2024-08-01",
-      time: "18:30:00",
-      status: "경기 종료",
-      homeTeam: {
-        id: 7,
-        name: "LG",
-      },
-      awayTeam: {
-        id: 4,
-        name: "삼성",
-      },
-      stadium: {
-        id: 1,
-        name: "잠실",
-        latitude: 0,
-        longitude: 0,
-        address: "no address",
-      },
-      homeTeamScore: 0,
-      awayTeamScore: 7,
-      winningTeam: {
-        id: 4,
-        name: "삼성",
-      },
-    },
-    cheeringTeam: {
-      id: 4,
-      name: "삼성",
-    },
-  },
-  {
-    id: 2,
-    image:
-      "https://i.pinimg.com/736x/32/d9/e4/32d9e459886530093d03cd44731a48eb.jpg",
-    seat: "116블록 3열 14번",
-    review: "별로였다",
-    status: "Lose",
-    game: {
-      id: "20240802SSLG0",
-      date: "2024-08-02",
-      time: "18:30:00",
-      status: "경기 종료",
-      homeTeam: {
-        id: 7,
-        name: "LG",
-      },
-      awayTeam: {
-        id: 4,
-        name: "삼성",
-      },
-      stadium: {
-        id: 1,
-        name: "잠실",
-        latitude: 0,
-        longitude: 0,
-        address: "no address",
-      },
-      homeTeamScore: 5,
-      awayTeamScore: 7,
-      winningTeam: {
-        id: 4,
-        name: "삼성",
-      },
-    },
-    cheeringTeam: {
-      id: 7,
-      name: "삼성",
-    },
-  },
-  {
-    id: 3,
-    image:
-      "https://i.pinimg.com/736x/4d/11/e1/4d11e1042915d1b825f6950892b7ad4d.jpg",
-    seat: "117블록 4열 15번",
-    review: "괜찮았다",
-    status: "Tie",
-    game: {
-      id: "20240803SSLG0",
-      date: "2024-08-03",
-      time: "18:30:00",
-      status: "경기 종료",
-      homeTeam: {
-        id: 7,
-        name: "LG",
-      },
-      awayTeam: {
-        id: 4,
-        name: "삼성",
-      },
-      stadium: {
-        id: 1,
-        name: "잠실",
-        latitude: 0,
-        longitude: 0,
-        address: "no address",
-      },
-      homeTeamScore: 3,
-      awayTeamScore: 3,
-      winningTeam: {
-        id: 0,
-        name: "무승부",
-      },
-    },
-    cheeringTeam: {
-      id: 4,
-      name: "삼성",
-    },
-  },
-  {
-    id: 4,
-    image: "http://example.com/url/to/image4.jpg",
-    seat: "118블록 5열 16번",
-    review: "관중 없는 경기",
-    status: "No game",
-    game: {
-      id: "20240804SSLG0",
-      date: "2024-08-04",
-      time: "18:30:00",
-      status: "경기 취소",
-      homeTeam: {
-        id: 7,
-        name: "LG",
-      },
-      awayTeam: {
-        id: 4,
-        name: "삼성",
-      },
-      stadium: {
-        id: 1,
-        name: "잠실",
-        latitude: 0,
-        longitude: 0,
-        address: "no address",
-      },
-      homeTeamScore: 0,
-      awayTeamScore: 0,
-      winningTeam: {
-        id: 0,
-        name: "경기 없음",
-      },
-    },
-    cheeringTeam: {
-      id: 7,
-      name: "삼성",
-    },
-  },
-];
-
 const WatchList = () => {
   const [activeSelect, setActiveSelect] = useState(0);
+  const [selcetMonth, setSelcetMonth] = useState(new Date());
+  const navigate = useNavigate();
+  const { data } = useQuery({
+    queryKey: ["registeredGame", selcetMonth],
+    queryFn: () =>
+      getRegisteredGameMonthly(
+        selcetMonth.getFullYear(),
+        selcetMonth.getMonth() + 1,
+      ),
+  });
+
+  const handleMonthChange = (date: Date) => {
+    setSelcetMonth(date);
+  };
+
+  const handleClickDay = (date: Date) => {
+    // TODO: 이게 맞는 방법일까
+    data?.forEach((item: MyGame) => {
+      if (item.game.date === moment(date).format("YYYY-MM-DD")) {
+        navigate(`/register/${item.id}`);
+      }
+    });
+  };
+
+  const handleClickMatch = (id: number) => {
+    navigate(`/register/${id}`);
+  };
+
   const renderContent = () => {
     switch (activeSelect) {
       case 0:
-        return <CalendarContainer data={DATA} />;
+        return (
+          <CalendarContainer
+            onClick={(date) => handleClickDay(date)}
+            data={data}
+            onMonthChange={handleMonthChange}
+          />
+        );
       case 1:
-        return <ListTab data={DATA} />;
+        return (
+          <ListTab
+            matches={data}
+            onClick={(match: MyGame) => handleClickMatch(match.id)}
+          />
+        );
       case 2:
-        return <GalleryTab data={DATA} />;
+        return (
+          <GalleryTab
+            onClick={(match: MyGame) => handleClickMatch(match.id)}
+            data={data}
+          />
+        );
       default:
         return null;
     }
   };
+
   return (
     <WatchListContainer>
       <div className='watchList-header'>
@@ -199,6 +90,7 @@ const WatchList = () => {
     </WatchListContainer>
   );
 };
+
 const WatchListContainer = styled.div`
   width: 100%;
   display: flex;
