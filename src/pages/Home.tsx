a52517aa4583adc83f8a61c0a121ef51796a4938
minseok <imuchgabis@gmail.com>
2024-08-20 11:52:44 +0900

    ✨ 온보딩 로고 추가
    온보딩 로고 추가

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,15 +2,13 @@
 import styled from "styled-components";
 import { useNavigate } from "react-router-dom";
 import Button from "../components/common/Button";
+import onBoarding from "../assets/images/onboarding/onBoarding.png";
 
 const Home = () => {
   const navigate = useNavigate();
   return (
     <Container>
-      <Image
-        src='https://static-cdn.sporki.com/news/sportschosun/20238/1000397/c_2023081801001381300185091.jpg'
-        alt='환영 이미지'
-      />
+      <Image src={onBoarding} alt='환영 이미지' />
       <Title>환영합니다!</Title>
       <Subtitle>우리팀의 승리를 책임지는 승리요정!</Subtitle>
       <Subtitle>지금 바로 시작해보세요.</Subtitle>
