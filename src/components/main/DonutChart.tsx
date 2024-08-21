d53935b6f40f582aae7396ce5562ece617cde157
namkijoo <rlwn5904@naver.com>
2024-08-22 01:30:30 +0900

    ✨ 승률 차트 생성

--- src/components/main/DonutChart.tsx
+++ src/components/main/DonutChart.tsx
@@ -1,14 +1,9 @@
 import { Doughnut } from "react-chartjs-2";
-import {
-  Chart as ChartJS,
-  ArcElement,
-  Tooltip,
-  Legend,
-  ChartOptions,
-} from "chart.js";
+import { Chart as ChartJS, ArcElement, ChartOptions } from "chart.js";
 import styled from "styled-components";
 import { Record } from "@/types/Record";
 import { useAuthStore } from "@/store/authStore";
+import Text from "../common/Text";
 
 const color = [
   ["#041E42", "#D00F31", "#898C9B", "#BABCC3"], // 1 롯데
@@ -23,7 +18,7 @@ const color = [
   ["#FF6600", "#25282A", "#898C9B", "#BABCC3"], // 10 한화
 ];
 
-ChartJS.register(ArcElement, Tooltip, Legend);
+ChartJS.register(ArcElement);
 
 interface DonutChartProps {
   record: Record;
@@ -55,11 +50,30 @@ const DonutChart = ({ record }: DonutChartProps) => {
     <ChartWrapper>
       <BarWrapper>
         <Doughnut data={data} options={options} />
+        <CenterText>
+          <Text variant='subtitle_01' color='var(--gray-400)'>
+            전체 경기 수
+          </Text>
+          <Text variant='headline'>{record.total}</Text>
+        </CenterText>
       </BarWrapper>
       <LegendWrapper>
-        {color.map((element, key) => {
-          return <div key={key}>{element}</div>;
-        })}
+        <LabelWrapper teamColor={color[teamId - 1][0]}>
+          <div />
+          <Text variant='subtitle_01'>:승</Text>
+        </LabelWrapper>
+        <LabelWrapper teamColor={color[teamId - 1][1]}>
+          <div />
+          <Text variant='subtitle_01'>:패</Text>
+        </LabelWrapper>
+        <LabelWrapper teamColor={color[teamId - 1][2]}>
+          <div />
+          <Text variant='subtitle_01'>:동점</Text>
+        </LabelWrapper>
+        <LabelWrapper teamColor={color[teamId - 1][3]}>
+          <div />
+          <Text variant='subtitle_01'>:경기 취소</Text>
+        </LabelWrapper>
       </LegendWrapper>
     </ChartWrapper>
   );
@@ -74,11 +88,42 @@ const ChartWrapper = styled.div`
   align-items: center;
 `;
 const BarWrapper = styled.div`
+  position: relative;
   width: 80%;
 `;
 
 const LegendWrapper = styled.div`
-  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  width: 90%;
+  margin: 20px 0;
+  align-items: center;
 `;
 
+const LabelWrapper = styled.div<{ teamColor: string }>`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  > div {
+    border-radius: 100%;
+    width: 12px;
+    height: 12px;
+    background-color: ${({ teamColor }) => teamColor};
+    margin-right: 5px;
+  }
+`;
+
+const CenterText = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  font-size: 20px;
+  font-weight: bold;
+  color: #000;
+`;
 export default DonutChart;
