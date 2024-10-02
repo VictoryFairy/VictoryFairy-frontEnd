0e5526e7c0a2bad12c0745ae6782e76514a53289
phj <aaagowns@gmail.com>
2024-10-02 09:23:57 +0900

    라우터 추적 ts추가

---
+++ src/RouteChangeTracker.ts
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import ReactGA from "react-ga4";
+
+const RouteChangeTracker = () => {
+  const location = useLocation();
+  const [initialized, setInitialized] = useState(false);
+
+  useEffect(() => {
+    if (import.meta.env.VITE_GA_ID) {
+      ReactGA.initialize(import.meta.env.VITE_GA_ID);
+      setInitialized(true);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (initialized) {
+      ReactGA.send({ hitType: "pageview", page: location.pathname });
+    }
+  }, [initialized, location]);
+};
+
+export default RouteChangeTracker;
