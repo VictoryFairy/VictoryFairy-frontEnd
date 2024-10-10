c81cde82231514a1747effb379b28d0ed4b0738d
minseok <imuchgabis@gmail.com>
2024-10-10 17:02:46 +0900

    ✨ Iframe 타이틀 추가
    iframe 타이틀 추가

--- src/pages/CheersongDetail.tsx
+++ src/pages/CheersongDetail.tsx
@@ -54,6 +54,7 @@ const CheersongDetail = () => {
           src={`https://www.youtube.com/embed/${videoId}`}
           allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
           allowFullScreen
+          title='응원가 비디오입니다.'
         />
       )}
       <Lyrics
