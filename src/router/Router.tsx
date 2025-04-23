bb3d576a2055952b2e1eee2559bcc66b751b7f7c
namkijoo <rlwn5904@naver.com>
2025-04-23 17:58:02 +0900

    fix:직관기록하기 폰트 굵기 수정

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -355,7 +355,7 @@ const Router = createBrowserRouter([
   },
   {
     path: "/register",
-    element: <Layout left={<BackButton />} center='직관기록' footer={false} />,
+    element: <Layout left={<BackButton />} center='직관 기록' footer={false} />,
     children: [
       {
         path: "/register",
