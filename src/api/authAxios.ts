79648ca9a8c75c7a73768eec080fe34f24f42397
namkijoo <rlwn5904@naver.com>
2025-04-29 01:39:41 +0900

    Feat:429에러 처리

--- src/api/authAxios.ts
+++ src/api/authAxios.ts
@@ -50,6 +50,10 @@ authAxiosInstance.interceptors.response.use(
         return Promise.reject(err);
       }
     }
+    if (response.status === 429) {
+      alert("요청이 너무 많습니다. 잠시 후 다시 시도해주세요.");
+      return Promise.reject(error);
+    }
 
     return Promise.reject(error);
   },
