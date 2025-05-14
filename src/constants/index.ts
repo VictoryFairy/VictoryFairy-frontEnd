335649affb2cab5ba359414ca4518faa27a9af5c
phjjj <aaagowns@gmail.com>
2025-05-15 00:46:20 +0900

    feat : 경기 상태 상수 추가

---
+++ src/constants/index.ts
@@ -0,0 +1,14 @@
+export const CanceledGameStatus = [
+  "우천취소",
+  "폭염취소",
+  "기타",
+  "미세먼지취소",
+  "그라운드사정",
+];
+
+export const GameStatus = [
+  "경기중",
+  "경기전",
+  "경기종료",
+  ...CanceledGameStatus,
+];
