08b6ccc4cf328d4ee81aa53863b998f5313af459
namkijoo <rlwn5904@naver.com>
2025-05-19 16:52:07 +0900

    Fix:이미지 다운로드 캐싱문제 해결

--- src/utils/getFairyImg.ts
+++ src/utils/getFairyImg.ts
@@ -3,14 +3,11 @@ export const getFairyImg = (
   teamId: number,
   format: "png" | "webp",
 ) => {
-  let fairyStatus = "";
+  const fairyStatus = rate > 30 ? "happy" : "sad";
 
-  if (rate > 30) {
-    fairyStatus = "happy";
-  } else {
-    fairyStatus = "sad";
-  }
+  // 현재 시간을 기반으로 캐시 무효화 쿼리 추가
+  const timestamp = Date.now();
+  const imgUrl = `https://sngyo-image.s3.ap-northeast-2.amazonaws.com/fairyImg/${fairyStatus}/${teamId}.${format}?v=${timestamp}`;
 
-  const imgUrl = `https://sngyo-image.s3.ap-northeast-2.amazonaws.com/fairyImg/${fairyStatus}/${teamId}.${format}`;
   return imgUrl;
 };
